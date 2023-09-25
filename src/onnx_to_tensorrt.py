416a16397ec392e32a52c923c9b26f6b7d616374
Dima Birenbaum <dvlasenko86@gmail.com>
2023-09-25 12:16:56 +0300

    Set precision mode using correct API matching tensorrt version

--- src/onnx_to_tensorrt.py
+++ src/onnx_to_tensorrt.py
@@ -4,15 +4,21 @@ import pycuda.driver as cuda
 # Initialize CUDA Driver
 cuda.init()
 
+
 def convert_onnx_to_tensorrt(onnx_file_path, engine_file_path, precision_mode):
     # Create a TensorRT logger
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 
     # Create a builder
-    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(1) as network, trt.OnnxParser(network,
-                                                                                                  TRT_LOGGER) as parser:
-        builder.max_workspace_size = 1 << 30  # 1GB
-        builder.fp16_mode = (precision_mode == "FP16")
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_builder_config() as config, builder.create_network(
+            1) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+
+        # Set the workspace size
+        config.max_workspace_size = 1 << 30  # 1GB
+
+        # Set the precision mode
+        if precision_mode == "FP16":
+            config.set_flag(trt.BuilderFlag.FP16)
 
         # Parse the ONNX model
         with open(onnx_file_path, 'rb') as model:
@@ -23,6 +29,6 @@ def convert_onnx_to_tensorrt(onnx_file_path, engine_file_path, precision_mode):
                 return
 
         # Build and save the engine
-        with builder.build_cuda_engine(network) as engine:
+        with builder.build_engine(network, config) as engine:
             with open(engine_file_path, 'wb') as f:
                 f.write(engine.serialize())
