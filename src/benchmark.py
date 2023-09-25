58a9f25bd7591cbce5e2f01f58f6e47a813f3d26
Dima Birenbaum <dvlasenko86@gmail.com>
2023-09-25 15:44:30 +0300

    Added FP16 and FP32

--- src/benchmark.py
+++ src/benchmark.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import torch.backends.cudnn as cudnn
 import torch
+from main import logging
 
 class Benchmark:
     def __init__(self, model, device="cuda", input_shape=(32, 3, 224, 224), dtype='fp32', nwarmup=50, nruns=100):
@@ -33,4 +34,4 @@ class Benchmark:
                     print('Iteration %d/%d, ave batch time %.2f ms' % (i, self.nruns, np.mean(timings) * 1000))
         print("Input shape:", input_data.size())
         print("Output features size:", features.size())
-        print('Average batch time: %.2f ms' % (np.mean(timings) * 1000))
+        logging.info('Average batch time: %.2f ms' % (np.mean(timings) * 1000))
--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ from image_processor import ImageProcessor
 from benchmark import Benchmark
 import torch
 import torch_tensorrt
+import logging
 
+# Set up logging
+logging.basicConfig(filename='model.log', level=logging.INFO)
 
 def main():
-    device = "cuda"
-    img_path = "../inference/cat.png"
-    topk = 5
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    img_path = "./inference/cat3.jpg"
+    topk = 2
 
     model_loader = ModelLoader(device=device)
     img_processor = ImageProcessor(img_path=img_path, device=device)
@@ -19,39 +22,40 @@ def main():
     for i in range(topk):
         probability = probs[i].item()
         class_label = model_loader.categories[0][int(classes[i])]
-        print("%{} {}".format(int(probability * 100), class_label))
+        logging.info("My prediction: %{} {}".format(int(probability * 100), class_label))
 
-    print("Running Benchmark for CPU")
+    logging.info("Running Benchmark for CPU")
     benchmark_cpu = Benchmark(model_loader.model.to("cpu"), device="cpu")
     benchmark_cpu.run()
 
-    print("Running Benchmark for CUDA")
+    logging.info("Running Benchmark for CUDA")
     benchmark_cuda = Benchmark(model_loader.model.to("cuda"), device="cuda")
     benchmark_cuda.run()
 
-    print("Tracing and Compiling CUDA model into TensorRT model")
+    print("Tracing CUDA model")
     traced_model = torch.jit.trace(model_loader.model, [torch.randn((32, 3, 224, 224)).to("cuda")])
-    trt_model = torch_tensorrt.compile(
-        traced_model,
-        inputs=[torch_tensorrt.Input((32, 3, 224, 224), dtype=torch.float32)],
-        enabled_precisions={torch.float32}
-    )
 
-    print("Running Benchmark for TensorRT")
-    benchmark_trt = Benchmark(trt_model, device="cuda")
-    benchmark_trt.run()
+    for precision in [torch.float32, torch.float16]:
+        logging.info(f"Compiling and Running Benchmark for TensorRT with precision: {precision}")
+        trt_model = torch_tensorrt.compile(
+            traced_model,
+            inputs=[torch_tensorrt.Input((32, 3, 224, 224), dtype=precision)],
+            enabled_precisions={precision}
+        )
+        benchmark_trt = Benchmark(trt_model, device="cuda")
+        benchmark_trt.run()
 
-    print("Making prediction with TensorRT model")
-    trt_model.eval()
-    with torch.no_grad():
-        outputs = trt_model(img_batch)
-    prob_trt = torch.nn.functional.softmax(outputs[0], dim=0)
+        print("Making prediction with TensorRT model")
+        trt_model.eval()
+        with torch.no_grad():
+            outputs = trt_model(img_batch)
+        prob_trt = torch.nn.functional.softmax(outputs[0], dim=0)
 
-    probs_trt, classes_trt = torch.topk(prob_trt, topk)
-    for i in range(topk):
-        probability = probs_trt[i].item()
-        class_label = model_loader.categories[0][int(classes_trt[i])]
-        print("%{} {}".format(int(probability * 100), class_label))
+        probs_trt, classes_trt = torch.topk(prob_trt, topk)
+        for i in range(topk):
+            probability = probs_trt[i].item()
+            class_label = model_loader.categories[0][int(classes_trt[i])]
+            print("%{} {}".format(int(probability * 100), class_label))
 
 
 if __name__ == "__main__":
