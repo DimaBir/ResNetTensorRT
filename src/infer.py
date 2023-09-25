234d216ab7c9fa6c450975874b4a8c055937e1bc
Dima Birenbaum <dvlasenko86@gmail.com>
2023-09-25 12:59:54 +0300

    Fixed Model loading

--- src/infer.py
+++ src/infer.py
@@ -6,10 +6,11 @@ from PIL import Image
 import tensorrt as trt
 import numpy as np
 import pycuda.driver as cuda
+import pycuda.autoinit
 
 # Check if CUDA is available and set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 
 def load_cifar10_images():
     # Load a few images from CIFAR10
@@ -71,36 +72,49 @@ def infer_with_pytorch_model(image, model_path):
 
 
 def infer_with_tensorrt_engine(image, engine_file_path):
-    # Initialize CUDA
+    # Initialize CUDA driver
     cuda.init()
 
-    # Load the TensorRT engine
-    TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-    with open(engine_file_path, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
-        engine = runtime.deserialize_cuda_engine(f.read())
+    # Get the first available GPU
+    gpu = cuda.Device(0)
 
-    # Convert the image to the format expected by TensorRT
-    image = image.cpu().numpy().astype(np.float32)  # Move image to the CPU
-    image = np.transpose(image, (1, 2, 0))
+    # Create a CUDA context
+    context = gpu.make_context()
 
-    # Allocate buffers and run inference
-    inputs, outputs, bindings, stream = [], [], [], cuda.Stream()
-    for binding in engine:
-        size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
-        dtype = trt.nptype(engine.get_binding_dtype(binding))
-        host_mem = cuda.pagelocked_empty(size, dtype)
-        cuda_mem = cuda.mem_alloc(host_mem.nbytes)
-        bindings.append(int(cuda_mem))
-        if engine.binding_is_input(binding):
-            inputs.append({'host': host_mem, 'device': cuda_mem})
-        else:
-            outputs.append({'host': host_mem, 'device': cuda_mem})
-    np.copyto(inputs[0]['host'], image.ravel())
-    cuda.memcpy_htod_async(inputs[0]['device'], inputs[0]['host'], stream)
-    engine(context, bindings=bindings, stream_handle=stream.handle)
-    cuda.memcpy_dtoh_async(outputs[0]['host'], outputs[0]['device'], stream)
-    stream.synchronize()
+    try:
+        # Load the TensorRT engine
+        with open(engine_file_path, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
+            engine = runtime.deserialize_cuda_engine(f.read())
 
-    # Get the predicted label
-    predicted_label = np.argmax(outputs[0]['host'])
-    return predicted_label
+        # Create execution context
+        with engine.create_execution_context() as exec_context:
+            # Allocate device memory
+            inputs, outputs, bindings = [], [], []
+            stream = cuda.Stream()
+            for binding in engine:
+                size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
+                dtype = trt.nptype(engine.get_binding_dtype(binding))
+                host_mem = cuda.pagelocked_empty(size, dtype)
+                cuda_mem = cuda.mem_alloc(host_mem.nbytes)
+                bindings.append(int(cuda_mem))
+                if engine.binding_is_input(binding):
+                    inputs.append({'host': host_mem, 'device': cuda_mem})
+                else:
+                    outputs.append({'host': host_mem, 'device': cuda_mem})
+
+            # Transfer input data to the GPU
+            np.copyto(inputs[0]['host'], image.ravel())
+            cuda.memcpy_htod_async(inputs[0]['device'], inputs[0]['host'], stream)
+
+            # Run inference
+            exec_context.execute_async(bindings=bindings, stream_handle=stream.handle)
+
+            # Transfer predictions back from the GPU
+            cuda.memcpy_dtoh_async(outputs[0]['host'], outputs[0]['device'], stream)
+            stream.synchronize()
+
+            # Return the result
+            return outputs[0]['host']
+    finally:
+        # Pop the CUDA context
+        context.pop()
