3f869e780b794b61a43e1a8eed263d7092db489a
Dima Birenbaum <dvlasenko86@gmail.com>
2023-10-16 12:21:56 +0300

    Refactored inference_base.py

--- src/inference_base.py
+++ src/inference_base.py
@@ -1,33 +1,51 @@
-import os
-import shutil
 import time
 import logging
 import numpy as np
 
 
 class InferenceBase:
-    def __init__(
-        self, model_loader, onnx_path=None, ov_path=None, topk=5, debug_mode=False
-    ):
-        self.model_loader = model_loader
-        self.model = self.model_loader.model
-        # self.model_path = model_loader.model_path
+    def __init__(self, model_loader, onnx_path=None, ov_path=None, topk=5, debug_mode=False):
+        """
+        Base class for inference.
 
+        :param model_loader: Object responsible for loading the model and categories.
+        :param onnx_path: Path to the ONNX model (if applicable).
+        :param ov_path: Path to the OpenVINO model (if applicable).
+        :param topk: Number of top predictions to return.
+        :param debug_mode: If True, print additional debug information.
+        """
+        self.model_loader = model_loader
         self.onnx_path = onnx_path
         self.ov_path = ov_path
-
         self.categories = model_loader.categories
         self.model = self.load_model()
         self.topk = topk
         self.debug_mode = debug_mode
 
     def load_model(self):
+        """
+        Load the model. This method should be implemented by subclasses.
+        """
         raise NotImplementedError
 
     def predict(self, input_data, is_benchmark=False):
+        """
+        Run prediction on the input data.
+
+        :param input_data: Data to run the prediction on.
+        :param is_benchmark: If True, the prediction is part of a benchmark run.
+        """
         logging.info(f"Running prediction for {self.__class__.__name__} model")
 
     def benchmark(self, input_data, num_runs=100, warmup_runs=50):
+        """
+        Benchmark the prediction performance.
+
+        :param input_data: Data to run the benchmark on.
+        :param num_runs: Number of runs for the benchmark.
+        :param warmup_runs: Number of warmup runs before the benchmark.
+        :return: Average inference time in milliseconds.
+        """
         # Warmup
         logging.info(f"Starting warmup for {self.__class__.__name__} inference...")
         for _ in range(warmup_runs):
@@ -38,20 +56,29 @@ class InferenceBase:
         start_time = time.time()
         for _ in range(num_runs):
             self.predict(input_data, is_benchmark=True)
-        avg_time = ((time.time() - start_time) / num_runs) * 1000  # To ms
+        avg_time = ((time.time() - start_time) / num_runs) * 1000  # Convert to ms
+
         logging.info(f"Average inference time for {num_runs} runs: {avg_time:.4f} ms")
         if self.debug_mode:
-            print(
-                f"Average inference time for {self.__class__.__name__} and {num_runs} runs: {avg_time:.4f} ms"
-            )
+            print(f"Average inference time for {self.__class__.__name__} and {num_runs} runs: {avg_time:.4f} ms")
         return avg_time
 
     def get_top_predictions(self, prob: np.ndarray, is_benchmark=False):
+        """
+        Get the top predictions based on the probabilities.
+
+        :param prob: Array of probabilities.
+        :param is_benchmark: If True, the method is called during a benchmark run.
+        :return: Array of probabilities.
+        """
         if is_benchmark:
             return None
 
-        top_indices = prob.argsort()[-self.topk :][::-1]
+        # Get the top indices and probabilities
+        top_indices = prob.argsort()[-self.topk:][::-1]
         top_probs = prob[top_indices]
+
+        # Log and print the top predictions
         for i in range(self.topk):
             probability = top_probs[i]
             class_label = self.categories[0][int(top_indices[i])]
