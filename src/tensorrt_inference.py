96ce3803a6576e81f58205ed3cc7c2f2cc90895d
Dima Birenbaum <dvlasenko86@gmail.com>
2023-10-16 12:45:09 +0300

    Refactored tensorrt_inference.py

--- src/tensorrt_inference.py
+++ src/tensorrt_inference.py
@@ -2,23 +2,33 @@ import torch
 import logging
 from src.inference_base import InferenceBase
 
-
-CUDA_AVAILABLE = False
-if torch.cuda.is_available():
+# Check for CUDA and TensorRT availability
+CUDA_AVAILABLE = torch.cuda.is_available()
+if CUDA_AVAILABLE:
     try:
         import torch_tensorrt as trt
-
-        CUDA_AVAILABLE = True
     except ImportError:
-        print("torch-tensorrt is not installed. Running on CPU mode only.")
+        logging.warning("torch-tensorrt is not installed. Running on CPU mode only.")
+        CUDA_AVAILABLE = False
 
 
 class TensorRTInference(InferenceBase):
     def __init__(self, model_loader, precision=torch.float32):
+        """
+        Initialize the TensorRTInference object.
+
+        :param model_loader: Object responsible for loading the model and categories.
+        :param precision: Precision mode for TensorRT (default is torch.float32).
+        """
         self.precision = precision
         super().__init__(model_loader)
+        if CUDA_AVAILABLE:
+            self.load_model()
 
     def load_model(self):
+        """
+        Load and convert the PyTorch model to TensorRT format.
+        """
         # Load the PyTorch model
         self.model = self.model_loader.model.to(self.device).eval()
 
@@ -28,16 +38,32 @@ class TensorRTInference(InferenceBase):
         )
 
     def predict(self, img_batch, topk: int):
+        """
+        Run prediction on the input data using the TensorRT model.
+
+        :param img_batch: Data to run the prediction on.
+        :param topk: Number of top predictions to return.
+        :return: Top predictions based on the probabilities.
+        """
         logging.info(
             f"Running prediction for TensorRT (CUDA) model with {self.precision} precision"
         )
         with torch.no_grad():
             outputs = self.model(img_batch.to(self.device))
-        # Post-processing and printing results
+
+        # Compute the softmax probabilities
         prob = torch.nn.functional.softmax(outputs[0], dim=0)
         prob = prob.cpu().numpy()
 
         return self.log_top_predictions(prob, topk)
 
     def benchmark(self, input_data, num_runs=100, warmup_runs=50):
-        super().benchmark(input_data, num_runs, warmup_runs)
+        """
+        Benchmark the prediction performance using the TensorRT model.
+
+        :param input_data: Data to run the benchmark on.
+        :param num_runs: Number of runs for the benchmark.
+        :param warmup_runs: Number of warmup runs before the benchmark.
+        :return: Average inference time in milliseconds.
+        """
+        return super().benchmark(input_data, num_runs, warmup_runs)
