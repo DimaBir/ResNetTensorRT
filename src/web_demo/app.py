f3dbae0948a69e9a6eb59dc059022484bfaf7cea
Dima Birenbaum <dvlasenko86@gmail.com>
2024-08-21 11:08:02 +0300

    Removing usage of SSL cert
    Signed-off-by: Dima Birenbaum <dvlasenko86@gmail.com>

--- src/web_demo/app.py
+++ src/web_demo/app.py
@@ -219,5 +219,5 @@ if __name__ == "__main__":
     # Configure logging
     logging.basicConfig(level=logging.INFO)
     app.run(
-        host="0.0.0.0", port=5000, ssl_context=(SSL_CERT_PATH, SSL_KEY_PATH), debug=True
+        host="0.0.0.0", port=5000, debug=True
     )
